d7bbf90ec412f3dfd937d0628bea3588ecd6ff90
contatofnog <27527497+contatofnog@users.noreply.github.com>
2019-04-18 20:33:05 -0300

    Add Arrays.php

---
+++ Exercises/Arrays.php
@@ -0,0 +1,46 @@
+<?php
+
+/*	Arrays - PHP (exercise)
+	github.com/contatofnog */
+
+// Numeric array
+
+echo "<h2>Numeric array</h2>";
+
+$numeric = array(1, 2, 3, 4);
+
+foreach($numeric as $key => $value) {
+   	echo "[$key] $value <br>";
+}
+
+// Associative array
+
+echo "<h2>Associative array</h2>";
+
+$assoc = array('a' => 5, 'b' => 6, 'c' => 7, 'd' => 8);
+
+foreach($assoc as $key => $value) {
+   	echo "[$key] $value <br>";
+}
+
+// Multidimensional array
+
+echo "<h2>Multidimensional array</h2>";
+
+$multi = array(
+	'fruits' => array(
+		'apple', 'banana', 'pineapple'
+	),
+
+	'juices' => array(
+		'orange', 'grape', 'strawberry'
+	)
+);
+
+foreach($multi['fruits'] as $key => $value) {
+	echo "[$key] $value <br>";
+}
+
+foreach($multi['juices'] as $key => $value) {
+	echo "<br>[$key] $value";
+}
