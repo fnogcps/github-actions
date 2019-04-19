52e2786b305e8a7315d0a8c12cec5397b31f53a4
contatofnog <27527497+contatofnog@users.noreply.github.com>
2019-04-19 02:20:36 -0300

    Add SolveMeFirst.php

---
+++ Exercises/HackerRank/SolveMeFirst.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+  * [HackerRank] Solve Me First (exercise)
+  * hackerrank.com/challenges/solve-me-first
+  * github.com/contatofnog
+*/
+
+function solveMeFirst($a,$b) {
+  return $a + $b;
+}
