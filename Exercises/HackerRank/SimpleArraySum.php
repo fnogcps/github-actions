4400343f9bc6c41fbaab6c97a9a7919fee342c17
contatofnog <27527497+contatofnog@users.noreply.github.com>
2019-04-19 02:56:34 -0300

    Add SimpleArraySum.php

---
+++ Exercises/HackerRank/SimpleArraySum.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+  * [HackerRank] Simple Array Sum (exercise)
+  * hackerrank.com/challenges/simple-array-sum
+  * github.com/contatofnog
+*/
+
+$ar = [1, 2, 3, 4, 10, 11];
+
+function simpleArraySum($ar) {
+    return array_sum($ar);
+}
