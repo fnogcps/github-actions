60ef8e065271fbf560bbbd963c7677f3ec452429
contatofnog <27527497+contatofnog@users.noreply.github.com>
2019-04-19 23:38:02 -0300

    Add CompareTheTriplets.php

---
+++ Exercises/HackerRank/CompareTheTriplets.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+  * [HackerRank] Compare the Triplets (exercise)
+  * hackerrank.com/challenges/compare-the-triplets
+  * github.com/contatofnog
+*/
+
+$a = [5, 6, 7];
+$b = [3, 6, 10];
+
+function compareTriplets($a, $b) {
+
+    $pointsA = 0;
+    $pointsB = 0;
+
+    for($i = 0; $i <= 2; $i++) {
+	    if ($a[$i] > $b[$i]) {
+	    	$pointsA++;
+	    }
+	    else if ($a[$i] < $b[$i]) {
+	    	$pointsB++;
+	    }
+	}
+    return [$pointsA, $pointsB];
+}
