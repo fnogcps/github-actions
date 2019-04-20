153330774a3e13270c6879e4e2328ad9728e7012
contatofnog <27527497+contatofnog@users.noreply.github.com>
2019-04-19 23:36:20 -0300

    Add SimpleCalculator.php

---
+++ Exercises/Misc/SimpleCalculator.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+  * [PHP] Simple Calculator (exercise)
+  * github.com/contatofnog
+*/
+
+function Addition($a, $b) {
+  return ($a + $b);
+}
+
+function Subtraction($a, $b) {
+  return ($a - $b);
+}
+
+function Multiplication($a, $b) {
+  return ($a * $b);
+}
+
+function Division($a, $b) {
+  return ($a / $b);
+}
+
+echo '<br><b>Addition: </b>'. Addition(10, 5);
+echo '<br><b>Subtraction: </b>'. Subtraction(10, 5);
+echo '<br><b>Multiplication: </b>'. Multiplication(10, 5);
+echo '<br><b>Division: </b>'. Division(10, 5);
