998d7c16a310ec139d5a28dc2f918f0637a231f3
Felipe Nogueira <27527497+contatofnog@users.noreply.github.com>
2019-04-24 22:44:12 -0300

    Add Staircase.php

---
+++ Exercises/HackerRank/Staircase.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+  * [HackerRank] Staircase (exercise)
+  * hackerrank.com/challenges/staircase
+  * github.com/contatofnog
+*/
+
+function staircase($n) {
+    $i = 1;
+    $j = $n;
+
+    while($i <= $n) {
+        $j--;
+        echo str_repeat(" ", $j) . str_repeat("#", $i) ."\n";
+        $i++;
+    }
+}
