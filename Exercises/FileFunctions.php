083630bea55d3c57bac27a22ecd17306df6936ac
Felipe Nogueira <27527497+contatofnog@users.noreply.github.com>
2019-04-25 23:19:54 -0300

    Add FileFunctions.php

---
+++ Exercises/FileFunctions.php
@@ -0,0 +1,34 @@
+<?php
+
+/*	File Functions - PHP (exercise)
+	github.com/contatofnog */
+
+$filename = '/srv/www/apache/php/FileFunctions.php';
+
+// file_exists()
+
+if(file_exists($filename)) {echo "$filename existe";}
+else {echo "$filename não existe";}
+
+// is_file()
+
+if(is_file($filename)) {echo "<br>$filename é um arquivo";}
+else {echo "<br>$filename não é um arquivo";}
+
+// mime_content_type()
+
+echo "<br>MIME: ". mime_content_type($filename);
+
+// basename()
+
+echo "<br>Nome do arquivo: ". basename($filename);
+
+// dirname()
+
+echo "<br>Diretório: ". dirname($filename);
+
+// pathinfo()
+
+echo "<br> Caminho: <pre>";
+var_dump(pathinfo($filename));
+echo "</pre>";
