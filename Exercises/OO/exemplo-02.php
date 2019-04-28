0c4556e9a2cd588bec6c79e52e2e507177c8fa47
Felipe Nogueira <27527497+contatofnog@users.noreply.github.com>
2019-04-27 22:40:43 -0300

    Add exemplo-02.php

---
+++ Exercises/OO/exemplo-02.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+  * [PHP] Modificadores de acesso - Visibilidade (exercício)
+  * github.com/contatofnog
+*/
+
+class Cachorro {
+  public $nome = "Rex";
+  private $raca = "Bull terrier";
+  protected $cor = "Branco";
+
+  public function exibirRaca() {
+    echo "<br> <b>[Raça]</b> $this->raca";
+  }
+}
+
+class outraClasse extends Cachorro {
+  public function exibirCor() {
+    echo "<br> <b>[Cor]</b> $this->cor";
+  }
+}
+
+$cachorro = new Cachorro();
+echo "<b>[Nome]</b> $cachorro->nome";
+$cachorro->exibirRaca();
+
+$cor = new outraClasse;
+$cor->exibirCor();
