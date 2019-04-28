7d5224ca0a1bc4a4b83b0d8b0d4f4117e38f1d4c
Felipe Nogueira <27527497+contatofnog@users.noreply.github.com>
2019-04-28 19:40:20 -0300

    Add exemplo-03.php

---
+++ Exercises/OO/exemplo-03.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+  * [PHP] Const, self e parent (exercício)
+  * github.com/contatofnog
+*/
+
+class Pessoa {
+  const nome = "Felipe";
+
+  public function exibirNome() {
+    echo self::nome;
+  }
+}
+
+class Felipe extends Pessoa {
+  const nome = "Nogueira";
+
+  public function exibirNome() {
+    echo '<br>'. parent::nome;
+  }
+}
+
+$pessoa = new Pessoa();
+$pessoa->exibirNome();
+
+$felipe = new Felipe();
+$felipe->exibirNome();
