fb0599b173749da97f38732ec1d7b4d58ef64398
Felipe Nogueira <27527497+contatofnog@users.noreply.github.com>
2019-05-02 19:17:43 -0300

    Add exemplo-07.php

---
+++ Exercises/OO/exemplo-07.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+  * [PHP] Interfaces (exercício)
+  * github.com/contatofnog
+*/
+
+interface Animal {
+  public function Comer();
+  public function Andar();
+  public function Correr();
+  public function Dormir();
+}
+
+class Cachorro implements Animal {
+  public function Comer() {
+    echo "O cachorro está comendo";
+  }
+
+  public function Andar() {
+    echo "O cachorro está andando";
+  }
+
+  public function Correr() {
+    echo "O cachorro está correndo";
+  }
+
+  public function Dormir() {
+    echo "O cachorro está dormindo";
+  }
+}
+
+$cachorro = new Cachorro;
+$cachorro->Comer();
