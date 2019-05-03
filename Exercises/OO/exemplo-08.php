3f6212ebe246f3e92def41b6baabe442190bf2f6
Felipe Nogueira <27527497+contatofnog@users.noreply.github.com>
2019-05-03 11:57:09 -0300

    Add exemplo-08.php

---
+++ Exercises/OO/exemplo-08.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+  * [PHP] Tratamento de exceções (exercício)
+  * github.com/contatofnog
+*/
+
+class Mercado {
+  public $saldo;
+ 
+  public function verificarSaldo() {
+    if($this->saldo < 5.0) {
+      throw new Exception("Saldo insuficiente!", 1);
+    }
+    else {
+      echo "Você possui saldo!";
+    }
+  }
+}
+
+$compra = new Mercado();
+$compra->saldo = 3.5;
+
+try {$compra->verificarSaldo();}
+catch (Exception $e) {
+  echo $e->getMessage();
+}
