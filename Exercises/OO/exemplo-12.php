17e48173b0548ff2cf77e8f3aa361070c4822a5b
Felipe Nogueira <27527497+contatofnog@users.noreply.github.com>
2019-05-05 22:58:02 -0300

    Add exemplo-12.php

---
+++ Exercises/OO/exemplo-12.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+  * [PHP] Associação (exercício)
+  * github.com/contatofnog
+*/
+
+class Usuario {
+  public $nome;
+  public $email;
+}
+
+class Newsletter {
+  public $numero;
+  public $freq;
+}
+
+$usuario = new Usuario();
+$usuario->nome = "Felipe";
+$usuario->email = "exemplo@exemplo.com";
+
+$newsletter = new Newsletter();
+$newsletter->usuario = $usuario;
+$newsletter->freq = 7;
+
+$dados = array(
+  'usuario' => $newsletter->usuario->nome,
+  'email' => $newsletter->usuario->email,
+  'freq' => $newsletter->freq
+);
+
+echo var_dump($dados);
