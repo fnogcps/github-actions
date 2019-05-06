0c01e8a152a1f14f14f15fab9e37baddf2eca7f7
Felipe Nogueira <27527497+contatofnog@users.noreply.github.com>
2019-05-05 22:00:47 -0300

    Add exemplo-11.php

---
+++ Exercises/OO/exemplo-11.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+  * [PHP] Agregação (exercício)
+  * github.com/contatofnog
+*/
+
+class Animal {
+  public $animal;
+  public $raca;
+
+  function __construct($animal, $raca) {
+    $this->animal = $animal;
+    $this->raca = $raca;
+  }
+}
+
+class Loja {
+  public $animais;
+
+  public function Adicionar(Animal $animal) {
+    $this->animais[] = $animal;
+  }
+
+  public function Exibir() {
+    foreach ($this->animais as $animal) {
+      echo "$animal->animal da raça $animal->raca <br>"; }
+  }
+}
+
+$gato = new Animal("Gato", "Siamês");
+$cachorro = new Animal("Cachorro", "Bulldog");
+
+$loja = new Loja();
+$loja->Adicionar($gato);
+$loja->Adicionar($cachorro);
+$loja->Exibir();
