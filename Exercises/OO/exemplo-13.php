363d1d4a6d2a33adb2e6e978c9633599de94a1b3
Felipe Nogueira <27527497+contatofnog@users.noreply.github.com>
2019-05-06 20:54:37 -0300

    Add exemplo-13.php

---
+++ Exercises/OO/exemplo-13.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+  * [PHP] Abstração (exercício)
+  * github.com/contatofnog
+*/
+
+abstract class Motor {
+    abstract protected function ligarMotor();
+    abstract protected function desligarMotor();
+    public function trocarPneus() {
+        echo "Trocando pneus ...<br>";
+    }
+}
+
+class motorCarro extends Motor {
+    public function ligarMotor() { echo "Ligando o motor do carro ...<br>"; }
+    public function desligarMotor() { echo "Desligando o motor do carro ...<br>"; }
+}
+
+$carro = new motorCarro();
+$carro->ligarMotor();
+$carro->desligarMotor();
+$carro->trocarPneus();
