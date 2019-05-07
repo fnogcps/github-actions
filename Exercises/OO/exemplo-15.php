7905f514154df51463c968ba5f2d4ff7c2507580
Felipe Nogueira <27527497+contatofnog@users.noreply.github.com>
2019-05-06 21:24:31 -0300

    Add exemplo-15.php

---
+++ Exercises/OO/exemplo-15.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+  * [PHP] Construtor (exercício)
+  * github.com/contatofnog
+*/
+
+class Login {
+  private $email, $senha, $nome;
+
+  public function __construct($email, $senha, $nome) {
+    $this->email = $email;
+    $this->senha = $senha;
+    $this->nome = $nome;
+  }
+
+  public function fazerLogin() {
+    if($this->email == "teste@teste.com" && $this->senha == "teste") {
+      echo "Olá $this->nome!";
+    }
+    else { echo "Dados inválidos!"; }
+  }
+}
+
+$usuario = new Login("teste@teste.com", "teste", "Felipe");
+$usuario->fazerLogin();
