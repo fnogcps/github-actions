23dba4c6baef7b6ad242b59c5839ccab65e5a6d7
Felipe Nogueira <27527497+contatofnog@users.noreply.github.com>
2019-05-06 21:04:48 -0300

    Add exemplo-14.php

---
+++ Exercises/OO/exemplo-14.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+  * [PHP] Herança (aula)
+  * github.com/contatofnog
+*/
+
+class Pessoa {
+  protected $nome;
+
+  public function atribuirNome($nome) {
+    $this->nome = $nome;
+    echo "Um nome foi atribuído!<br>";
+  }
+
+  protected function fazerIntervalo() {
+    echo "$this->nome fez uma pausa!<br>";
+  }
+}
+
+class Aluno extends Pessoa {
+  public function iniciarEstudos() {
+    echo "$this->nome começou a estudar!<br>";
+    $this->fazerIntervalo();
+  }
+}
+
+$aluno = new Aluno();
+$aluno->atribuirNome("Felipe");
+$aluno->iniciarEstudos();
