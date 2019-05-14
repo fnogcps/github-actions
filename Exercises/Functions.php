335bb5812d79ba448014d734504b5e6972656e9a
Felipe Nogueira <27527497+contatofnog@users.noreply.github.com>
2019-05-14 07:48:45 -0300

    Update Functions.php

--- Exercises/Functions.php
+++ Exercises/Functions.php
@@ -1,35 +1,39 @@
 <?php
 
 /*
-  * [PHP] Functions (exercise)
+  * [PHP] Funções (exercício)
   * github.com/contatofnog
 */
 
-function Incrementa(&$a) {
-  $a += 10;
+function Incrementa(&$a)
+{
+    $a += 10;
 }
 
 $a = 90;
 Incrementa($a);
 echo "<b>Incrementa():</b> $a <br>";
 
-function Ola() {
-  $argumentos = func_get_args();
-  $quantidade = func_num_args();
+function Ola()
+{
+    $argumentos = func_get_args();
+    $quantidade = func_num_args();
 
-  for($i = 0; $i < $quantidade; $i++) {
-    echo '<br>Olá '. $argumentos[$i] .'!';
-  }
+    for($i = 0; $i < $quantidade; $i++) {
+        echo '<br>Olá '. $argumentos[$i] .'!';
+    }
 }
 
 Ola('João', 'Maria', 'José', 'Pedro');
 echo '<br>';
 
-function Fatorial($numero) {
-    if ($numero == 1)
-      return $numero;
-    else
-      return $numero * Fatorial($numero - 1);
+function Fatorial($numero)
+{
+    if ($numero == 1) {
+        return $numero;
+    } else {
+        return $numero * Fatorial($numero - 1);
+    }
 }
 
 echo '<br> <b>Fatorial (5):</b>'. Fatorial(5);
