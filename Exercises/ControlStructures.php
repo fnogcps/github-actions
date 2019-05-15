658911d9d22d6668820fea0dbfb70bccb54165f5
Felipe Nogueira <27527497+contatofnog@users.noreply.github.com>
2019-05-14 23:55:31 -0300

    Update ControlStructures.php

--- Exercises/ControlStructures.php
+++ Exercises/ControlStructures.php
@@ -1,127 +1,116 @@
 <?php
 
 /*
-  * [PHP] Control Structures (exercise)
+  * [PHP] Estruturas de controle (exercício)
   * github.com/contatofnog
 */
 
 $a = 10;
 $b = 25;
 
-echo "Value (a): $a";
-echo "<br>Value (b): $b";
+echo "Valor (a): $a";
+echo "<br>Valor (b): $b";
 
-echo "<h2>If, else & elseif statements</h2>";
+echo "<h1>Estruturas de condição</h1>";
 
-// If statement
+// If, else if, else
+echo "<h2>If, else if, else</h2>";
 
 if ($a > $b) {
-	echo '$a is greater than $b';
+    echo "$a é maior que $b";
+} else if ($a < $b) {
+    echo "$a é menor que $b";
+} else {
+    echo "$a é igual a $b";
 }
 
-// Else if statement
-
-else if ($a < $b) {
-	echo '$a is less than $b';
-}
-
-// Else if statement
-
-else {
-	echo '$a is equal to $b';
-}
-
-// While statement
-
-echo "<h2>While statement</h2>";
-
-$i = 0;
-
-while ($i <= 10) {
-	echo "<b>[While]</b> $i <br>";
-	$i++;
-}
-
-// do-while statement
-
-echo "<h2>do-while statement</h2>";
-
-$i = 0;
-
-do {
-	$i++;
-	echo "<b>[do-while]</b> $i <br>";
-} while ($i >= 10);
-
-// For statement
-
-echo "<h2>For statement</h2>";
-
-for ($i=0; $i <= 5; $i++) { 
-	echo "<b>[For]</b> $i <br>";
-}
-
-// Foreach statement
-
-echo "<h2>Foreach statement</h2>";
-
-$arr = array(1, 2, 3, 4);
-
-foreach ($arr as $key => $value) {
-	echo "<b>[Foreach]</b> $key | $value <br>";
-}
-
-// Switch statement
-
-echo "<h2>Switch statement</h2>";
+// Switch
+echo "<h2>Switch</h2>";
 
 $i = 3;
 
 switch ($i) {
-	case '1':
-		echo "<b>[Case]</b> 1";
-		break;
-	case '2':
-		echo "<b>[Case]</b> 2";
-		break;
-	case '3':
-		echo "<b>[Case]</b> 3";
-		break;
-	default:
-		echo "<b>[Case]</b> ?";
-		break;
+case '1':
+    echo "<b>[Caso]</b> 1";
+    break;
+case '2':
+    echo "<b>[Caso]</b> 2";
+    break;
+case '3':
+    echo "<b>[Caso]</b> 3";
+    break;
+default:
+    echo "<b>[Caso]</b> ?";
+    break;
 }
 
-// Continue statement
+echo "<h1>Estruturas de repetição</h1>";
 
-echo '<h2>Continue statement</h2>';
+$i = 0;
+
+// While
+while ($i <= 10) {
+    echo "<b>[While]</b> $i <br>";
+    $i++;
+}
+
+// do-while
+echo "<h2>do-while</h2>";
+
+$i = 0;
+
+do {
+    $i++;
+    echo "<b>[do-while]</b> $i <br>";
+} while ($i >= 10);
+
+// For
+echo "<h2>For</h2>";
+
+for ($i=0; $i <= 5; $i++) { 
+    echo "<b>[For]</b> $i <br>";
+}
+
+// Foreach
+echo "<h2>Foreach</h2>";
+
+$arr = array(1, 2, 3, 4);
+
+foreach ($arr as $indice => $valor) {
+    echo "<b>[Foreach]</b> $indice | $valor <br>";
+}
+
+echo "<h1>Outras estruturas de controle</h1>";
+
+// Continue
+echo '<h2>Continue</h2>';
 
 $i = 0;
 
 while ($i < 5) {
-	$i++;
-	if ($i == 3) { continue; }
-	echo "<b>[Continue]</b> $i <br>";
+    $i++;
+    if ($i == 3) { continue; 
+    }
+    echo "<b>[Continue]</b> $i <br>";
 }
 
-// Break statement
-
-echo '<h2>Break statement</h2>';
+// Break
+echo '<h2>Break</h2>';
 
 $i = 0;
 
 while ($i < 5) {
-	$i++;
-	echo "<b>[Break]</b> $i <br>";
-	if ($i == 3) { break; }
+    $i++;
+    echo "<b>[Break]</b> $i <br>";
+    if ($i == 3) { break; 
+    }
 }
 
-// Goto statement
+// Goto
+echo '<h2>Goto</h2>';
 
-echo '<h2>Goto statement</h2>';
-
-goto example;
+goto exemplo;
 echo '<b>Hello,</b>';
 
-example:
+exemplo:
 echo '<b>World!</b>';
