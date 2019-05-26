b8e8f48eef8e1f7339aac3defa5c164534b818d7
Felipe Nogueira <27527497+contatofnog@users.noreply.github.com>
2019-05-25 21:51:00 -0300

    Update ControlStructures.php

--- PHP/ControlStructures.php
+++ PHP/ControlStructures.php
@@ -1,89 +1,79 @@
 <?php
 
-/*
-  * [PHP] Estruturas de controle (exercício)
-  * github.com/contatofnog
-*/
-
 $a = 10;
 $b = 25;
 
-echo "Valor (a): $a";
-echo "<br>Valor (b): $b";
+print "Valor (a): $a\n";
+print "Valor (b): $b\n";
 
-echo "<h1>Estruturas de condição</h1>";
+// Estruturas de controle
 
-// If, else if, else
-echo "<h2>If, else if, else</h2>";
+print "\nExemplo de If, else if, else:\n\n";
 
 if ($a > $b) {
-    echo "$a é maior que $b";
+    print "$a é maior que $b";
 } else if ($a < $b) {
-    echo "$a é menor que $b";
+    print "$a é menor que $b";
 } else {
-    echo "$a é igual a $b";
+    print "$a é igual a $b";
 }
 
-// Switch
-echo "<h2>Switch</h2>";
+print "\n\nExemplo de switch:\n\n";
 
 $i = 3;
 
 switch ($i) {
 case '1':
-    echo "<b>[Caso]</b> 1";
+    print "(caso) 1";
     break;
 case '2':
-    echo "<b>[Caso]</b> 2";
+    print "(caso) 2";
     break;
 case '3':
-    echo "<b>[Caso]</b> 3";
+    print "(caso) 3";
     break;
 default:
-    echo "<b>[Caso]</b> ?";
+    print "(caso) default";
     break;
 }
 
-echo "<h1>Estruturas de repetição</h1>";
+// Estruturas de repetição
 
 $i = 0;
 
-// While
+print "\n\nExemplo de while:\n\n";
+
 while ($i <= 10) {
-    echo "<b>[While]</b> $i <br>";
+    print "(while) $i \n";
     $i++;
 }
 
-// do-while
-echo "<h2>do-while</h2>";
+print "\nExemplo de do-while:\n\n";
 
 $i = 0;
 
 do {
     $i++;
-    echo "<b>[do-while]</b> $i <br>";
-} while ($i >= 10);
+    print "(do-while) $i \n";
+} while ($i <= 3);
 
-// For
-echo "<h2>For</h2>";
+print "\nExemplo de for:\n\n";
 
 for ($i=0; $i <= 5; $i++) { 
-    echo "<b>[For]</b> $i <br>";
+    print "(for) $i \n";
 }
 
-// Foreach
-echo "<h2>Foreach</h2>";
+print "\nExemplo de foreach:\n\n";
 
 $arr = array(1, 2, 3, 4);
 
 foreach ($arr as $indice => $valor) {
-    echo "<b>[Foreach]</b> $indice | $valor <br>";
+    print "(foreach) $indice | $valor \n";
 }
 
-echo "<h1>Outras estruturas de controle</h1>";
+// Outras estruturas de controle
 
-// Continue
-echo '<h2>Continue</h2>';
+print "\nExemplo de continue:\n\n";
 
 $i = 0;
 
@@ -91,26 +81,24 @@ while ($i < 5) {
     $i++;
     if ($i == 3) { continue; 
     }
-    echo "<b>[Continue]</b> $i <br>";
+    print "(continue) $i \n";
 }
 
-// Break
-echo '<h2>Break</h2>';
+print "\nExemplo de break:\n\n";
 
 $i = 0;
 
 while ($i < 5) {
     $i++;
-    echo "<b>[Break]</b> $i <br>";
+    print "(break) $i \n";
     if ($i == 3) { break; 
     }
 }
 
-// Goto
-echo '<h2>Goto</h2>';
+print "\nExemplo de goto:\n\n";
 
 goto exemplo;
-echo '<b>Hello,</b>';
+print 'Olá,';
 
 exemplo:
-echo '<b>World!</b>';
+print 'Mundo!';
