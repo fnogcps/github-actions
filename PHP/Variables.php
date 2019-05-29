b8c411ab1a943b23115028666e3b703521a682a2
Felipe Nogueira <27527497+contatofnog@users.noreply.github.com>
2019-05-29 20:00:35 -0300

    Update Variables.php

--- PHP/Variables.php
+++ PHP/Variables.php
@@ -1,56 +1,39 @@
 <?php
 
-/*
-  * [PHP] Variáveis (exercício)
-  * github.com/contatofnog
-*/
-
 // Tipos de dados
-echo "<h2>Tipos de dados</h2>";
 
 $int = 10;                // Inteiro
 $float = 2.0;             // Ponto flutuante
 $bool = false;            // Lógico
 $null = null;             // Nulo
-$str = 'Hello, World!';   // String
-$arr = array(1, 2, 3);    // Array
-
-echo '<b>Inteiro:</b> ', var_dump($int);
-echo '<br> <b>Ponto flutuante:</b> ', var_dump($float);
-echo '<br> <b>Lógico:</b> ', var_dump($bool);
-echo '<br> <b>Nulo:</b> ', var_dump($null);
-echo '<br> <b>String:</b> ', var_dump($str);
-echo '<br> <b>Array:</b> ', var_dump($arr);
+$string = 'Olá, Mundo!';  // String
+$array = array(1, 2, 3);  // Array
 
 // Variáveis variáveis
-echo "<h2>Variáveis variáveis</h2>";
 
 $foo = 'baz';
 $$foo = 'bar'; 
 
 echo '$foo: '. $foo;
-echo '<br>$$foo ($baz): '. $baz;
+echo PHP_EOL;
+echo '$$foo ($baz): '. $baz;
 
 // Atribuição por referência
-echo "<h2>Atribuição por referência</h2>";
 
 $foo = 25;
 $bar = &$foo; 
 
-echo '$foo: '. $foo;
-echo '<br>$bar: '. $bar .
-'<br>';
+echo PHP_EOL;
+
+echo PHP_EOL .'$foo: '. $foo;
+echo PHP_EOL .'$bar: '. $bar;
+
+echo PHP_EOL;
 
 $foo = 30;
 
-echo '<br>$foo: '. $foo;
-echo '<br>$bar: '. $bar;
-
-// tipo Resource -> Resources.php
-// tipo Object -> Objects.php
-
-// Escopo das variáveis
-echo "<h2>Escopo das variáveis</h2>";
+echo PHP_EOL .'$foo: '. $foo;
+echo PHP_EOL .'$bar: '. $bar;
 
 // Escopo global
 $sobrenome = 'Silva';
@@ -66,4 +49,4 @@ function atribuirNome()
 }
 
 atribuirNome();
-echo 'Nome: ', $nome;
+echo "\n\nNome: ", $nome;
