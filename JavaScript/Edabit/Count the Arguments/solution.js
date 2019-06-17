90459ee880b2594ecaaa46e7e3eb2aca5ce1a7f0
Felipe Nogueira <contato.fnog@gmail.com>
2019-06-17 14:22:24 -0300

    Add unit tests to the "Count the Arguments" solution

--- JavaScript/Edabit/Count the Arguments/solution.js
+++ JavaScript/Edabit/Count the Arguments/solution.js
@@ -1,1 +1,2 @@
 function numArgs() { return arguments.length }
+module.exports = { numArgs }
---
+++ JavaScript/Edabit/Count the Arguments/tests.js
@@ -0,0 +1,37 @@
+var chai = require('chai')
+var assert = chai.assert
+
+const e = require('./solution.js')
+
+describe('Count the Arguments (unit tests)\n', () => {
+    it('[Test 1] Expected: 0', () => 
+        assert.equal(e.numArgs(), 0)
+    )
+    it('[Test 2] Expected: 1', () => 
+        assert.equal(e.numArgs('foo'), 1)
+    )
+    it('[Test 3] Expected: 2', () => 
+        assert.equal(e.numArgs('foo', 'bar'), 2)
+    )
+    it('[Test 4] Expected: 2', () => 
+        assert.equal(e.numArgs(null, null), 2)
+    )
+    it('[Test 5] Expected: 4', () => 
+        assert.equal(e.numArgs('foo', 'bar', 5, null), 4)
+    )
+    it('[Test 6] Expected: 1', () => 
+        assert.equal(e.numArgs(false), 1)
+    )
+    it('[Test 7] Expected: 2', () => 
+        assert.equal(e.numArgs('foo', {}), 2)
+    )
+    it('[Test 8] Expected: 3', () => 
+        assert.equal(e.numArgs('foo', 'bar', {}), 3)
+    )
+    it('[Test 9] Expected: 3', () => 
+        assert.equal(e.numArgs([], [], {}), 3)
+    )
+    it('[Test 10] Expected: 5', () => 
+        assert.equal(e.numArgs('foo', 'bar', true, null, 0), 5)
+    )
+})
