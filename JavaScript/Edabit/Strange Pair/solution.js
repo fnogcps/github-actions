1122a94f762ee824de831ddd00582dd998ce4b82
Felipe Nogueira <contato.fnog@gmail.com>
2019-06-16 22:38:16 -0300

    Add unit tests to the "Strange Pair" solution

--- JavaScript/Edabit/Strange Pair/solution.js
+++ JavaScript/Edabit/Strange Pair/solution.js
@@ -1,2 +1,4 @@
 const isStrangePair = (x, y) => 
     x.charAt(x.length - 1) == y.charAt() && y.charAt(y.length - 1) == x.charAt()
+
+module.exports = { isStrangePair }
---
+++ JavaScript/Edabit/Strange Pair/tests.js
@@ -0,0 +1,55 @@
+var chai = require('chai')
+var assert = chai.assert
+
+const e = require('./solution.js')
+
+describe('Strange Pair (unit tests)\n', () => {
+    it('[Test 1] Expected: true', () => 
+        assert.equal(e.isStrangePair("ratio", "orator"), true)
+    )
+    it('[Test 2] Expected: true', () => 
+        assert.equal(e.isStrangePair("sparkling", "groups"), true)
+    )
+    it('[Test 3] Expected: true', () => 
+        assert.equal(e.isStrangePair("mentee", "eminem"), true)
+    )
+    it('[Test 4] Expected: true', () => 
+        assert.equal(e.isStrangePair("yahtzee", "easy"), true)
+    )
+    it('[Test 5] Expected: false', () => 
+        assert.equal(e.isStrangePair("bush", "hubris"), false)
+    )
+    it('[Test 6] Expected: false', () => 
+        assert.equal(e.isStrangePair("edit", "cheese"), false)
+    )
+    it('[Test 7] Expected: false', () => 
+        assert.equal(e.isStrangePair("false", "true"), false)
+    )
+    it('[Test 8] Expected: false', () => 
+        assert.equal(e.isStrangePair("cupid", "dionysus"), false)
+    )
+    it('[Test 9] Expected: false', () => 
+        assert.equal(e.isStrangePair("futile", "elephant"), false)
+    )
+    it('[Test 10] Expected: true', () => 
+        assert.equal(e.isStrangePair("", ""), true)
+    )
+    it('[Test 11] Expected: false', () => 
+        assert.equal(e.isStrangePair("", "abc"), false)
+    )
+    it('[Test 12] Expected: true', () => 
+        assert.equal(e.isStrangePair("a", "a"), true)
+    )
+    it('[Test 13] Expected: false', () => 
+        assert.equal(e.isStrangePair("a", "b"), false)
+    )
+    it('[Test 14] Expected: true', () => 
+        assert.equal(e.isStrangePair("&", "&"), true)
+    )
+    it('[Test 15] Expected: true', () => 
+        assert.equal(e.isStrangePair("&!", "!&"), true)
+    )
+    it('[Test 16] Expected: true', () => 
+        assert.equal(e.isStrangePair("5556", "65"), true)
+    )
+})
