279ac4224f2a92ee3822ae069a567c6efb120e84
Felipe Nogueira <contato.fnog@gmail.com>
2019-06-16 22:31:33 -0300

    Add unit tests to the "Slice of Pie" solution

--- JavaScript/Edabit/Slice of Pie/solution.js
+++ JavaScript/Edabit/Slice of Pie/solution.js
@@ -1,1 +1,2 @@
 const equalSlices = (total, people, each) => ((people * each) <= total)
+module.exports = { equalSlices }
---
+++ JavaScript/Edabit/Slice of Pie/tests.js
@@ -0,0 +1,28 @@
+var chai = require('chai')
+var assert = chai.assert
+
+const e = require('./solution.js')
+
+describe('Slice of Pie (unit tests)\n', () => {
+    it('[Test 1] Expected: true', () => 
+        assert.equal(e.equalSlices(8, 3, 2), true)
+    )
+    it('[Test 2] Expected: false', () => 
+        assert.equal(e.equalSlices(8, 3, 3), false)
+    )
+    it('[Test 3] Expected: true', () => 
+        assert.equal(e.equalSlices(24, 12, 2), true)
+    )
+    it('[Test 4] Expected: false', () => 
+        assert.equal(e.equalSlices(5, 6, 1), false)
+    )
+    it('[Test 5] Expected: true', () => 
+        assert.equal(e.equalSlices(5, 0, 100), true)
+    )
+    it('[Test 6] Expected: true', () => 
+        assert.equal(e.equalSlices(15, 2, 7), true)
+    )
+    it('[Test 7] Expected: false', () => 
+        assert.equal(e.equalSlices(15, 2, 8), false)
+    )
+})
