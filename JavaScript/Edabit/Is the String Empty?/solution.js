503272d5dea775ef2f317f71ba8e82dad688d71b
Felipe Nogueira <contato.fnog@gmail.com>
2019-06-17 14:04:27 -0300

    Add unit tests to the "Is the String Empty?" solution

--- JavaScript/Edabit/Is the String Empty?/solution.js
+++ JavaScript/Edabit/Is the String Empty?/solution.js
@@ -1,1 +1,2 @@
 const isEmpty = s => (s.length === 0)
+module.exports = { isEmpty }
---
+++ JavaScript/Edabit/Is the String Empty?/tests.js
@@ -0,0 +1,15 @@
+var chai = require('chai')
+var assert = chai.assert
+
+const e = require('./solution.js')
+
+describe('Is the String Empty? (unit tests)\n', () => {
+    it('[Test 1] Expected: true', () => assert.equal(e.isEmpty(""), true))
+    it('[Test 2] Expected: false', () => assert.equal(e.isEmpty(" "), false))
+    it('[Test 3] Expected: false', () => 
+        assert.equal(e.isEmpty("            "), false)
+    )
+    it('[Test 4] Expected: false', () => assert.equal(e.isEmpty("38215"), false))
+    it('[Test 5] Expected: false', () => assert.equal(e.isEmpty("afjabsdf"), false))
+    it('[Test 6] Expected: false', () => assert.equal(e.isEmpty("!?@&"), false))
+})
