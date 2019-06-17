4e7a3b9d02b7958c04452aa721184b42344a2e36
Felipe Nogueira <contato.fnog@gmail.com>
2019-06-17 14:19:11 -0300

    Add unit tests to the "Find the Index" solution

--- JavaScript/Edabit/Find the Index/solution.js
+++ JavaScript/Edabit/Find the Index/solution.js
@@ -1,1 +1,2 @@
 const find_index = (arr, str) => arr.indexOf(str)
+module.exports = { find_index }
---
+++ JavaScript/Edabit/Find the Index/tests.js
@@ -0,0 +1,23 @@
+var chai = require('chai')
+var assert = chai.assert
+
+const e = require('./solution.js')
+
+describe('Find the Index (unit tests)\n', () => {
+    it('[Test 1] Expected: 3', () => 
+        assert.equal(
+            e.find_index(['abc', 'ghj', 'banana', 'grape'], 'grape'), 3)
+    )
+    it('[Test 2] Expected: 5', () => 
+        assert.equal(
+            e.find_index(['a', 'b', 'c', 'd', 'e', 'f'], 'f'), 5)
+    )
+    it('[Test 3] Expected: 1', () => 
+        assert.equal(
+            e.find_index(
+                ['hi', 'edabit.com', 'testcase', 'validstring'],
+                'edabit.com'
+            ),
+        1)
+    )
+})
