2ee71eac8c37438d14e796a2806062955125a92d
Felipe Nogueira <contato.fnog@gmail.com>
2019-06-17 13:57:43 -0300

    Add unit tests to the "Is the Number Less than or Equal to Zero?" solution

--- JavaScript/Edabit/Is the Number Less than or Equal to Zero?/solution.js
+++ JavaScript/Edabit/Is the Number Less than or Equal to Zero?/solution.js
@@ -1,1 +1,2 @@
 const lessThanOrEqualToZero = num => (num <= 0)
+module.exports = { lessThanOrEqualToZero }
---
+++ JavaScript/Edabit/Is the Number Less than or Equal to Zero?/tests.js
@@ -0,0 +1,28 @@
+var chai = require('chai')
+var assert = chai.assert
+
+const e = require('./solution.js')
+
+describe('Is the Number Less than or Equal to Zero? (unit tests)\n', () => {
+    it('[Test 1] Expected → false', () => 
+        assert.equal(e.lessThanOrEqualToZero(5), false)
+    )
+    it('[Test 2] Expected → true', () => 
+        assert.equal(e.lessThanOrEqualToZero(0), true)
+    )
+    it('[Test 3] Expected → true', () => 
+        assert.equal(e.lessThanOrEqualToZero(-5), true)
+    )
+    it('[Test 4] Expected → false', () => 
+        assert.equal(e.lessThanOrEqualToZero(1), false)
+    )
+    it('[Test 5] Expected → false', () => 
+        assert.equal(e.lessThanOrEqualToZero(2), false)
+    )
+    it('[Test 6] Expected → false', () => 
+        assert.equal(e.lessThanOrEqualToZero(10000), false)
+    )
+    it('[Test 7] Expected → false', () => 
+        assert.equal(e.lessThanOrEqualToZero(0.5), false)
+    )
+})
