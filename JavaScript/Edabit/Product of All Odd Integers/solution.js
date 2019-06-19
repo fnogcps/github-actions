cbc859bad5ad02df93330f88c7bb401c64ea5b2e
Felipe Nogueira <contato.fnog@gmail.com>
2019-06-19 12:53:10 -0300

    Add unit tests to the "Product of All Odd Integers" solution

--- JavaScript/Edabit/Product of All Odd Integers/solution.js
+++ JavaScript/Edabit/Product of All Odd Integers/solution.js
@@ -1,1 +1,2 @@
 const oddProduct = a => a.filter(a => (a % 2)).reduce((a, b) => a * b)
+module.exports = { oddProduct }
---
+++ JavaScript/Edabit/Product of All Odd Integers/tests.js
@@ -0,0 +1,22 @@
+var chai = require('chai')
+var assert = chai.assert
+
+const e = require('./solution.js')
+
+describe('Product of All Odd Integers (unit tests)\n', () => {
+    it('[Test 1] Expected: 15', () => 
+        assert.equal(e.oddProduct([3, 4, 1, 1, 5]), 15)
+    )
+    it('[Test 2] Expected: 25', () => 
+        assert.equal(e.oddProduct([5, 5, 8, 2, 4, 32]), 25)
+    )
+    it('[Test 3] Expected: 1', () => 
+        assert.equal(e.oddProduct([1, 2, 1, 2, 1, 2, 1, 2]), 1)
+    )
+    it('[Test 4] Expected: 1', () => 
+        assert.equal(e.oddProduct([0, 0, 0, 1]), 1)
+    )
+    it('[Test 5] Expected: 5', () => 
+        assert.equal(e.oddProduct([1, 2, 2, 5, 2, 0]), 5)
+    )
+})
