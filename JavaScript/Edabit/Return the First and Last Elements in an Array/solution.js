d45cf99b24b55ce65def875f13e98adf4f5d5fa1
Felipe Nogueira <contato.fnog@gmail.com>
2019-06-19 11:29:12 -0300

    Add unit tests to the "Return the First and Last Elements in an Array" solution

--- JavaScript/Edabit/Return the First and Last Elements in an Array/solution.js
+++ JavaScript/Edabit/Return the First and Last Elements in an Array/solution.js
@@ -1,1 +1,2 @@
 const firstLast = arr => [arr.shift(), arr.pop()]
+module.exports = { firstLast }
---
+++ JavaScript/Edabit/Return the First and Last Elements in an Array/tests.js
@@ -0,0 +1,49 @@
+var chai = require('chai')
+var assert = chai.assert
+
+const e = require('./solution.js')
+
+describe('Return the First and Last Elements in an Array (unit tests)\n', () => {
+    it('[Test 1] Expected: [5, 25]', () => 
+        assert.deepEqual(
+            e.firstLast([5, 10, 15, 20, 25]), 
+            [5, 25]
+        )
+    )
+    it('[Test 2] Expected: ["edabit", true]', () => 
+        assert.deepEqual(
+            e.firstLast(["edabit", 13, null, false, true]), 
+            ["edabit", true]
+        )
+    )
+    it('[Test 3] Expected: [undefined, null]', () => 
+        assert.deepEqual(
+            e.firstLast([undefined, 4, "6", "hello", null]), 
+            [undefined, null]
+        )
+    )
+    it('[Test 4] Expected: ["hello", "com"]', () => 
+        assert.deepEqual(
+            e.firstLast(["hello", "edabit", "dot", "com"]), 
+            ["hello", "com"]
+        )
+    )
+    it('[Test 5] Expected: [3, 1]', () => 
+        assert.deepEqual(
+            e.firstLast([3, 2, 1]), 
+            [3, 1]
+        )
+    )
+    it('[Test 6] Expected: ["one", "two"]', () => 
+        assert.deepEqual(
+            e.firstLast(["one", "two"]), 
+            ["one", "two"]
+        )
+    )
+    it('[Test 7] Expected: [false, false]', () => 
+        assert.deepEqual(
+            e.firstLast([false, false, true, false, false, true, false]), 
+            [false, false]
+        )
+    )
+})
