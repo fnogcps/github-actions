2e399f64292be77864c6588ddca86550b83bd21b
Felipe Nogueira <contato.fnog@gmail.com>
2019-06-20 18:51:31 -0300

    Add unit tests to the "Exists a Number Higher?" solution

--- JavaScript/Edabit/Exists a Number Higher?/solution.js
+++ JavaScript/Edabit/Exists a Number Higher?/solution.js
@@ -1,1 +1,2 @@
 const existsHigher = (arr, n) => arr.some(y => y >= n)
+module.exports = { existsHigher }
---
+++ JavaScript/Edabit/Exists a Number Higher?/tests.js
@@ -0,0 +1,22 @@
+var chai = require('chai')
+var assert = chai.assert
+
+const e = require('./solution.js')
+
+describe('Exists a Number Higher? (unit tests)\n', () => {
+    it('[Test 1] Expected: true', () => 
+        assert.equal(e.existsHigher([5, 3, 15, 22, 4], 10), true)
+    )
+    it('[Test 2] Expected: false', () => 
+        assert.equal(e.existsHigher([1, 2, 3, 4, 5], 8), false)
+    )
+    it('[Test 3] Expected: true', () => 
+        assert.equal(e.existsHigher([4, 3, 3, 3, 2, 2, 2], 4), true)
+    )
+    it('[Test 4] Expected: false', () => 
+        assert.equal(e.existsHigher([2, 2, 2, 2], 5), false)
+    )
+    it('[Test 5] Expected: false', () => 
+        assert.equal(e.existsHigher([], 0), false)
+    )
+})
