b05f9347e43301884391ea17301638afb0759fe4
Felipe Nogueira <contato.fnog@gmail.com>
2019-06-20 18:48:36 -0300

    Add unit tests to the "Find the Bug: Checking Even Numbers" solution

--- JavaScript/Edabit/Find the Bug: Checking Even Numbers/solution.js
+++ JavaScript/Edabit/Find the Bug: Checking Even Numbers/solution.js
@@ -1,1 +1,2 @@
 const checkAllEven = arr => arr.every(x => x % 2 === 0)
+module.exports = { checkAllEven }
---
+++ JavaScript/Edabit/Find the Bug: Checking Even Numbers/tests.js
@@ -0,0 +1,19 @@
+var chai = require('chai')
+var assert = chai.assert
+
+const e = require('./solution.js')
+
+describe('Find the Bug: Checking Even Numbers (unit tests)\n', () => {
+    it('[Test 1] Expected: false', () => 
+        assert.equal(e.checkAllEven([1, 2, 3, 4]), false)
+    )
+    it('[Test 2] Expected: true', () => 
+        assert.equal(e.checkAllEven([2, 4, 6]), true)
+    )
+    it('[Test 3] Expected: false', () => 
+        assert.equal(e.checkAllEven([5, 6, 8, 10]), false)
+    )
+    it('[Test 4] Expected: true', () => 
+        assert.equal(e.checkAllEven([-2, 2, -2, 2]), true)
+    )
+})
