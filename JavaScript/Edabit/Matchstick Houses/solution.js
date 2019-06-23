0ea5d1c0b6e24fe19254c65879ab714e3bdb9633
Felipe Nogueira <contato.fnog@gmail.com>
2019-06-23 17:27:41 -0300

    Add "Matchstick Houses" solution

---
+++ JavaScript/Edabit/Matchstick Houses/solution.js
@@ -0,0 +1,11 @@
+/*
+ * Matchstick Houses - Edabit
+ *
+ * This challenge will help you interpret mathematical relationships
+ * both algebraically and geometrically.
+ *
+ * https://edabit.com/challenge/tYHkTdFrEmWfxpPKF
+ * github.com/fnogcps
+ */
+
+matchHouses = a => a && a * 5 + 1;
