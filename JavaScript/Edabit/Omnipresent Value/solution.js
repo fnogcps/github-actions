46336937fba621950e855886a278da51bb5de6bd
Felipe Nogueira <contato.fnog@gmail.com>
2019-06-23 23:56:13 -0300

    Add "Omnipresent Value" solution

---
+++ JavaScript/Edabit/Omnipresent Value/solution.js
@@ -0,0 +1,10 @@
+/*
+ * Omnipresent Value - Edabit
+ *
+ * A value is omnipresent if it exists in every subarray inside the main array.
+ *
+ * https://edabit.com/challenge/Twbmxzd3JwkkqAWES
+ * github.com/fnogcps
+ */
+
+isOmnipresent = (a, b) => a.every(c => c.includes(b));
