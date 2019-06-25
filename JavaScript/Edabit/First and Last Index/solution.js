5114fc230ed162b5e275b6de8816693bffcb5160
Felipe Nogueira <contato.fnog@gmail.com>
2019-06-24 22:51:52 -0300

    Add unit tests to the "First and Last..." solution

--- JavaScript/Edabit/First and Last Index/solution.js
+++ JavaScript/Edabit/First and Last Index/solution.js
@@ -1,12 +1,14 @@
 /*
  * First and Last Index - Edabit
  *
- * Given a word, write a function that returns the first index 
+ * Given a word, write a function that returns the first index
  * and the last index of a character.
  *
  * https://edabit.com/challenge/AYpxyzbnbS7BGSgvd
  * github.com/fnogcps
  */
 
-charIndex = (a, b) => 
-    a.lastIndexOf(b) < 0 ? undefined : [a.indexOf(b), a.lastIndexOf(b)];
+charIndex = (a, b) =>
+  a.includes(b) ? [a.indexOf(b), a.lastIndexOf(b)] : void 0;
+
+module.exports = { charIndex };
