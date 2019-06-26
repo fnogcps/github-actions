019b1c3d3a17cba336a84f45f4184092980b4194
Felipe Nogueira <contato.fnog@gmail.com>
2019-06-25 22:01:33 -0300

    Add unit tests to the "Reverse the Case" solution

--- JavaScript/Edabit/Reverse the Case/solution.js
+++ JavaScript/Edabit/Reverse the Case/solution.js
@@ -1,12 +1,14 @@
 /*
  * Reverse the Case - Edabit
  *
- * Given a string, create a function to reverse the case. 
+ * Given a string, create a function to reverse the case.
  * All lower-cased letters should be upper-cased, and vice versa.
  *
  * https://edabit.com/challenge/PTvYKkvf7oytHmJFm
  * github.com/fnogcps
  */
 
-reverseCase = a => [...a].map(a => 
-    (a == a.toLowerCase() ? a.toUpperCase() : a.toLowerCase())).join("")
+reverseCase = a =>
+  [...a]
+    .map(a => (a == a.toLowerCase() ? a.toUpperCase() : a.toLowerCase()))
+    .join("");
---
+++ JavaScript/Edabit/Reverse the Case/test.js
@@ -0,0 +1,19 @@
+var chai = require('chai')
+var assert = chai.assert
+
+const e = require('./solution.js')
+
+describe('Reverse the Case (unit tests)\n', () => {
+    it('[Test 1] Expected: "hAPPY bIRTHDAY"', () =>
+        assert.strictEqual(e.reverseCase('Happy Birthday'), 'hAPPY bIRTHDAY')
+    )
+    it('[Test 2] Expected: "many thanks"', () =>
+        assert.strictEqual(e.reverseCase('MANY THANKS'), 'many thanks')
+    )
+    it('[Test 3] Expected: "SpOnTaNeOuS"', () =>
+        assert.strictEqual(e.reverseCase('sPoNtAnEoUs'), 'SpOnTaNeOuS')
+    )
+    it('[Test 4] Expected: "Excellent, YoUr Majesty"', () =>
+        assert.strictEqual(e.reverseCase('eXCELLENT, yOuR mAJESTY'), 'Excellent, YoUr Majesty')
+    )
+})
