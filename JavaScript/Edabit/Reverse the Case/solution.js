5e8620b3b2c499cb5728a9f0df94f01b03717b9b
Felipe Nogueira <contato.fnog@gmail.com>
2019-06-25 21:46:28 -0300

    Add "Reverse the Case" solution

---
+++ JavaScript/Edabit/Reverse the Case/solution.js
@@ -0,0 +1,12 @@
+/*
+ * Reverse the Case - Edabit
+ *
+ * Given a string, create a function to reverse the case. 
+ * All lower-cased letters should be upper-cased, and vice versa.
+ *
+ * https://edabit.com/challenge/PTvYKkvf7oytHmJFm
+ * github.com/fnogcps
+ */
+
+reverseCase = a => [...a].map(a => 
+    (a == a.toLowerCase() ? a.toUpperCase() : a.toLowerCase())).join("")
