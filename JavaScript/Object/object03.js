cc0255d0412d25ae26ba87f3b08915c67f84a320
Felipe Nogueira <contato.fnog@gmail.com>
2019-06-28 12:13:16 -0300

    Add object03.js

---
+++ JavaScript/Object/object03.js
@@ -0,0 +1,11 @@
+// Object initializer
+
+const x = 1,
+  y = 3,
+  z = 5;
+
+const objectOne = { x: x, y: y, z: z };
+console.log(objectOne);
+
+const objectTwo = { x, y, z };
+console.log(objectTwo);
