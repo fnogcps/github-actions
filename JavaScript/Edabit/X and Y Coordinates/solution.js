1757e10ef056104df50f1a03210017bf7824f568
Felipe Nogueira <contato.fnog@gmail.com>
2019-06-29 05:01:19 -0300

    Add "X and Y Coordinates" solution

---
+++ JavaScript/Edabit/X and Y Coordinates/solution.js
@@ -0,0 +1,11 @@
+/*
+ * X and Y Coordinates - Edabit
+ *
+ * Create a function that converts two arrays of x- and y- coordinates
+ * into an array of (x,y) coordinates.
+ *
+ * https://edabit.com/challenge/qYAhaD8fTbHWNe9oT
+ * github.com/fnogcps
+ */
+
+convertCartesian = (a, b) => a.map(a => [a, b]);
