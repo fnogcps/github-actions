1f234059ba2cffc11ebcae71b0a75751ccd4438b
Felipe Nogueira <contato.fnog@gmail.com>
2019-06-29 04:39:17 -0300

    Add "Find the Largest Number in an Array" solution

---
+++ JavaScript/Edabit/Find the Largest Number in an Array/solution.js
@@ -0,0 +1,11 @@
+/*
+ * Find the Largest Number in an Array - Edabit
+ *
+ * Create a function that takes an array of numbers.
+ * Return the largest number in the array.
+ *
+ * https://edabit.com/challenge/HTbRYCmPwfZGnBcxW
+ * github.com/fnogcps
+ */
+
+findLargestNum = a => Math.max(...a)
