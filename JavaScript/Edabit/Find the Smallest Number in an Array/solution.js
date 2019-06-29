ec44bb35b661da384266f640389ae897f940a0b2
Felipe Nogueira <contato.fnog@gmail.com>
2019-06-29 04:25:08 -0300

    Add "Find the Smallest Number in an A..." solution

---
+++ JavaScript/Edabit/Find the Smallest Number in an Array/solution.js
@@ -0,0 +1,11 @@
+/*
+ * Find the Smallest Number in an Array - Edabit
+ *
+ * Create a function that takes an array of numbers
+ * and returns the smallest number in the set.
+ *
+ * https://edabit.com/challenge/g66EJsbdnSDayyEcK
+ * github.com/fnogcps
+ */
+
+findSmallestNum = a => Math.min(...a);
