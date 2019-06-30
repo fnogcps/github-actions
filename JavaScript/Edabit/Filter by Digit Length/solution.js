feffdd8d927c58032141258df087c4e793894199
Felipe Nogueira <contato.fnog@gmail.com>
2019-06-30 04:17:20 -0300

    Add "Filter by Digit Length" solution

---
+++ JavaScript/Edabit/Filter by Digit Length/solution.js
@@ -0,0 +1,11 @@
+/*
+ * Filter by Digit Length - Edabit
+ *
+ * Create a function that filters out an array to include numbers
+ * who only have a certain number of digits.
+ *
+ * https://edabit.com/challenge/RXNybvGHLuvFiWKvq
+ * github.com/fnogcps
+ */
+
+filterDigitLength = (a, b) => a.filter(a => (a + '').length == b);
