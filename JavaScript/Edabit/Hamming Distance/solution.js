5a2b7d5f8f1c9d0ddae6077b355858d1bedd7c6c
Felipe Nogueira <contato.fnog@gmail.com>
2019-06-30 05:59:58 -0300

    Add "Hamming Distance" solution

---
+++ JavaScript/Edabit/Hamming Distance/solution.js
@@ -0,0 +1,13 @@
+/*
+ * Hamming Distance - Edabit
+ *
+ * Create a function that computes the hamming distance between two strings.
+ *
+ * https://edabit.com/challenge/vCtepsfrE7Nts2a9j
+ * github.com/fnogcps
+ */
+
+hammingDistance = (a, b, c = 0) => {
+  for (i in a) [...a][i] != [...b][i] && c++;
+  return c;
+};
