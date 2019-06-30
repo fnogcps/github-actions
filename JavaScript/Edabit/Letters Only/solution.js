065d3c3aac215241d64f703caec7348b0086aaf0
Felipe Nogueira <contato.fnog@gmail.com>
2019-06-30 06:29:43 -0300

    Add "Letters Only" solution

---
+++ JavaScript/Edabit/Letters Only/solution.js
@@ -0,0 +1,11 @@
+/*
+ * Letters Only - Edabit
+ *
+ * Write a function that removes any non-letters from a string,
+ * returning a well-known film title.
+ *
+ * https://edabit.com/challenge/GPKkNbytRpsAnHcrX
+ * github.com/fnogcps
+ */
+
+lettersOnly = a => a.replace(/[^a-z]/gi, '');
