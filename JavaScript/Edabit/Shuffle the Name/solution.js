637b399bbc877f8697949d76782a180683318fb2
Felipe Nogueira <contato.fnog@gmail.com>
2019-06-30 06:36:59 -0300

    Add "Shuffle the Name" solution

---
+++ JavaScript/Edabit/Shuffle the Name/solution.js
@@ -0,0 +1,15 @@
+/*
+ * Shuffle the Name - Edabit
+ *
+ * Create a function that accepts a string (of a persons first and last name)
+ * and returns a string with the first and last name swapped.
+ *
+ * https://edabit.com/challenge/eitMRpuJR3Ls4zQsn
+ * github.com/fnogcps
+ */
+
+nameShuffle = a =>
+  a
+    .split(' ')
+    .reverse()
+    .join(' ');
