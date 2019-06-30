cb138bd65ad481491a5650844dc47743df9efc88
Felipe Nogueira <contato.fnog@gmail.com>
2019-06-30 04:29:07 -0300

    Add "Multiply by Length" solution

---
+++ JavaScript/Edabit/Multiply by Length/solution.js
@@ -0,0 +1,11 @@
+/*
+ * Multiply by Length - Edabit
+ *
+ * Create a function to multiply all values in an array
+ * by the amount of values in that array.
+ *
+ * https://edabit.com/challenge/LMhRTq3hccz2D9Lax
+ * github.com/fnogcps
+ */
+
+MultiplyByLength = a => a.map(b => a.length * b);
