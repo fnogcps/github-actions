eb88622db8baf4edc901cda1835c569f1255f456
Felipe Nogueira <contato.fnog@gmail.com>
2019-06-30 05:43:38 -0300

    Add "Product Divisible By Sum?" solution

---
+++ JavaScript/Edabit/Product Divisible By Sum?/solution.js
@@ -0,0 +1,11 @@
+/*
+ * Product Divisible By Sum? - Edabit
+ *
+ * Write a function that returns true if the product
+ * of an array is divisible by the sum of that same array.
+ *
+ * https://edabit.com/challenge/aJzvBZgp8nzwSk94A
+ * github.com/fnogcps
+ */
+
+divisible = a => !(a.reduce(a => a * a) % a.reduce(a => a + a));
