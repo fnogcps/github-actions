3daebbe6a6906f8da133bf3ee9c4da86f352b47f
Felipe Nogueira <contato.fnog@gmail.com>
2019-06-30 03:49:52 -0300

    Add array02.js

---
+++ JavaScript/Array/array02.js
@@ -0,0 +1,11 @@
+// Array.shift, Array.unshift
+
+arrOne = [5, 10, 20];
+arrTwo = [40, 80, 160];
+
+arrOne.shift();
+console.log('arrOne:', arrOne);
+
+arrTwo.unshift(20);
+console.log('arrTwo:', arrTwo);
+console.log(arrTwo.shift());
