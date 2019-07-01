10ec6322380d482488f5331a9cc955a3f4882a56
Felipe Nogueira <contato.fnog@gmail.com>
2019-07-01 07:00:23 -0300

    Add "Typing Game" solution

---
+++ JavaScript/Edabit/Typing Game/solution.js
@@ -0,0 +1,13 @@
+/*
+ * Typing Game - Edabit
+ *
+ * Create a function that takes in two arrays:
+ * the array of user-typed words, and the array of correctly-typed words
+ * and outputs an array containing 1s (correctly-typed words)
+ * and -1s (incorrectly-typed words).
+ *
+ * https://edabit.com/challenge/KESyfS3A5vB2jkxFR
+ * github.com/fnogcps
+ */
+
+correctStream = (a, b) => a.map((_, c) => (a[c] == b[c] ? 1 : -1));
