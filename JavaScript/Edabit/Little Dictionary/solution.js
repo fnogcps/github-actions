acdeab12bef3cb4c0e35e9e12160870fc1c7122f
Felipe Nogueira <contato.fnog@gmail.com>
2019-07-01 06:29:53 -0300

    Add "Little Dictionary" solution

---
+++ JavaScript/Edabit/Little Dictionary/solution.js
@@ -0,0 +1,11 @@
+/*
+ * Little Dictionary - Edabit
+ *
+ * Create a function that takes in an initial word and filters out an array
+ * to contain words that start with the same letters as the initial word.
+ *
+ * https://edabit.com/challenge/p5xNwq2v9ZX7vo3mb
+ * github.com/fnogcps
+ */
+
+dictionary = (a, b) => b.filter(b => b.startsWith(a));
