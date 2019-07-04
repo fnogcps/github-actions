0477ff2bfd9643d7f7bfb773a0eb3fc4c59689a4
Felipe Nogueira <contato.fnog@gmail.com>
2019-07-03 23:32:45 -0300

    Add "Alphabet Soup" solution

---
+++ JavaScript/Edabit/Alphabet Soup/solution.js
@@ -0,0 +1,11 @@
+/*
+ * Alphabet Soup - Edabit
+ *
+ * Create a function that takes a string
+ * and returns a string with its letters in alphabetical order.
+ *
+ * https://edabit.com/challenge/bHDbMWpdeNaw23jqT
+ * github.com/fnogcps
+ */
+
+AlphabetSoup = a => [...a].sort().join('');
