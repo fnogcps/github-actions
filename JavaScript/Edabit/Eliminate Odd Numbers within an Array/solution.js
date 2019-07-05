6915a8e633794a911aa272a5e9b01232333708fa
Felipe Nogueira <contato.fnog@gmail.com>
2019-07-05 20:05:47 -0300

    Add "Eliminate Odd Numbers within an ..." solution

---
+++ JavaScript/Edabit/Eliminate Odd Numbers within an Array/solution.js
@@ -0,0 +1,11 @@
+/*
+ * Eliminate Odd Numbers within an Array - Edabit
+ *
+ * Create a function that takes an array of numbers
+ * and returns only the even values.
+ *
+ * https://edabit.com/challenge/uJ9K3HtbjHoDGXKhg
+ * github.com/fnogcps
+ */
+
+noOdds = a => a.filter(a => !(a % 2));
