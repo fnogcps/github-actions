f42889135fea6d4af7fd3d772242511e8fb729f3
Felipe Nogueira <contato.fnog@gmail.com>
2019-07-04 21:14:45 -0300

    Add "Edaaaaabit" solution

---
+++ JavaScript/Edabit/Edaaaaabit/solution.js
@@ -0,0 +1,11 @@
+/*
+ * Edaaaaabit - Edabit
+ *
+ * Write a function that takes an integer and returns
+ * a string with the given number of "a"s in Edabit.
+ *
+ * https://edabit.com/challenge/tHZBvD3w69hpDCtMg
+ * github.com/fnogcps
+ */
+
+howManyTimes = a => `Ed${'a'.repeat(a)}bit`;
