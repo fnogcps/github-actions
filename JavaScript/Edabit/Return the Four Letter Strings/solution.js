1c7890ef1d090beb9a63282bdb928b7fc1ca39b4
Felipe Nogueira <contato.fnog@gmail.com>
2019-07-05 20:11:42 -0300

    Add "Return the Four Letter Strings" solution

---
+++ JavaScript/Edabit/Return the Four Letter Strings/solution.js
@@ -0,0 +1,11 @@
+/*
+ * Return the Four Letter Strings - Edabit
+ *
+ * Create a function that takes an array of strings.
+ * Return all words in the array that are exactly four letters.
+ *
+ * https://edabit.com/challenge/havnWL4BimvoaXNMn
+ * github.com/fnogcps
+ */
+
+isFourLetters = a => a.filter(a => a.length == 4);
