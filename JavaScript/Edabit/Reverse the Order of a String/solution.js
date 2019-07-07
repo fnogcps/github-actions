9bb5d07738cb87d3ab7c84b6416b9648d65dbeaa
Felipe Nogueira <contato.fnog@gmail.com>
2019-07-06 22:31:07 -0300

    Add "Reverse the Order of a String" solution

---
+++ JavaScript/Edabit/Reverse the Order of a String/solution.js
@@ -0,0 +1,16 @@
+/*
+ * Reverse the Order of a String - Edabit
+ *
+ * Create a function that takes a string as its argument
+ * and returns the string in reversed order.
+ *
+ * https://edabit.com/challenge/oS3wCwLWsDqvs7XKd
+ * github.com/fnogcps
+ */
+
+reverse = a =>
+  a
+    .split('')
+    .reverse()
+    .join('');
+
