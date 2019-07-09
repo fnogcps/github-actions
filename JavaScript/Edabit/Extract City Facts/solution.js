77bf6089361d3f96d65e3847b7701f053a1f62fd
Felipe Nogueira <contato.fnog@gmail.com>
2019-07-08 23:37:18 -0300

    Add "Extract City Facts" solution

---
+++ JavaScript/Edabit/Extract City Facts/solution.js
@@ -0,0 +1,12 @@
+/*
+ * Extract City Facts - Edabit
+ *
+ * Create a function that takes an object as an argument
+ * and returns a string with facts about the city.
+ *
+ * https://edabit.com/challenge/FzDAgSR84zeyRa278
+ * github.com/fnogcps
+ */
+
+cityFacts = a =>
+  `${a.name} has a population of ${a.population} and is situated in ${a.continent}`;
