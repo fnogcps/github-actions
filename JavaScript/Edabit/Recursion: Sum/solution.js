e6c0fe1ab4fa9819c521cb14e9fa15f8976d9f60
Felipe Nogueira <contato.fnog@gmail.com>
2019-07-12 17:18:48 -0300

    Add "Recursion: Sum" solution

---
+++ JavaScript/Edabit/Recursion: Sum/solution.js
@@ -0,0 +1,17 @@
+/*
+ * Recursion: Sum - Edabit
+ *
+ * Write a function that recursively
+ * finds the sum of the first n natural numbers.
+ *
+ * https://edabit.com/challenge/uYQs3cBqPEsk9mdZB
+ * github.com/fnogcps
+ */
+
+sum = (a, c = []) => {
+  for (b = a; b > 0; b--) {
+    c.push(b);
+  }
+
+  return c.reduce((a, b) => a + b);
+};
