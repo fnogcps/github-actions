fcb5705f0c2b681b23d60a83c12cb88909d5740b
Felipe Nogueira <contato.fnog@gmail.com>
2019-07-12 18:17:51 -0300

    Add "Repeat the Same Item Multiple Times" solution

---
+++ JavaScript/Edabit/Repeat the Same Item Multiple Times/solution.js
@@ -0,0 +1,13 @@
+/*
+ * Repeat the Same Item Multiple Times - Edabit
+ *
+ * Create a function that takes two arguments (item, times).
+ * The first argument (item) is the item that needs repeating
+ * while the second argument (times) is the number of times the
+ * item is to be repeated. Return the result in an array.
+ *
+ * https://edabit.com/challenge/BEHiM4RTmhcLX7Cq6
+ * github.com/fnogcps
+ */
+
+repeat = (a, b) => Array(b).fill(a);
