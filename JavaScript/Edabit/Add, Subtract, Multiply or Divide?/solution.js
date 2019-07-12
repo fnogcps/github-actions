55bc0472e199ce5bbc9d1a3a8b8dcbec25d8378f
Felipe Nogueira <contato.fnog@gmail.com>
2019-07-12 17:32:55 -0300

    Add "Add, Subtract, Multiply or Divide?" solution

---
+++ JavaScript/Edabit/Add, Subtract, Multiply or Divide?/solution.js
@@ -0,0 +1,22 @@
+/*
+ * Add, Subtract, Multiply or Divide? - Edabit
+ *
+ * Write a function that takes two numbers and returns
+ * if they should be added, subtracted, multiplied or
+ * divided to get 24.
+ *
+ * If none of the operations can give 24, return null.
+ *
+ *  https://edabit.com/challenge/XyPee4TdK64BTgvbD
+ * github.com/fnogcps
+ */
+
+operation = (a, b) => {
+  return (
+    (a + b == 24 && 'added') ||
+    (a - b == 24 && 'subtracted') ||
+    (a * b == 24 && 'multiplied') ||
+    (a / b == 24 && 'divided') ||
+    null
+  );
+};
