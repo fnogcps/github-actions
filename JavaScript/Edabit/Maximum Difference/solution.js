f5bed0cffa434094d301c022c59c89df89fd3a3f
Felipe Nogueira <contato.fnog@gmail.com>
2019-07-13 22:44:39 -0300

    Add "Maximum Difference" solution

---
+++ JavaScript/Edabit/Maximum Difference/solution.js
@@ -0,0 +1,11 @@
+/*
+ * Maximum Difference - Edabit
+ *
+ * Given an array of integers, return the difference between
+ * the largest and smallest integers in the array.
+ *
+ * https://edabit.com/challenge/4vvFr9ZTK2AdyswXH
+ * github.com/fnogcps
+ */
+
+difference = a => Math.max(...a) - Math.min(...a);
