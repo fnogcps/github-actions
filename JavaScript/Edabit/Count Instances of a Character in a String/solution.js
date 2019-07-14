cd78f55c51b45b718713fdf5c1ea90b20ea1a79f
Felipe Nogueira <contato.fnog@gmail.com>
2019-07-13 23:27:37 -0300

    Add "Count Instances of a Character i..." solution

---
+++ JavaScript/Edabit/Count Instances of a Character in a String/solution.js
@@ -0,0 +1,11 @@
+/*
+ * Count Instances of a Character in a String - Edabit
+ *
+ * Create a function that takes two strings as arguments and returns
+ * the number of times the first string is found in the second string.
+ *
+ * https://edabit.com/challenge/kbFhwaDyrd79JrgeB
+ * github.com/fnogcps
+ */
+
+charCount = (a, b) => --b.split(a).length;
