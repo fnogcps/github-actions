ed3291d12e8555a0f65c685a48718c677d3a0a8d
Felipe Nogueira <contato.fnog@gmail.com>
2019-07-13 22:31:42 -0300

    Add "Recursion: Length of a String" solution

---
+++ JavaScript/Edabit/Recursion: Length of a String/solution.js
@@ -0,0 +1,11 @@
+/*
+ * Recursion: Length of a String - Edabit
+ *
+ * Write a function that returns the length of a string.
+ * Make your function recursive.
+ *
+ * https://edabit.com/challenge/4MSbtYFBiRtxHEkY8
+ * github.com/fnogcps
+ */
+
+length = a => a.length;
