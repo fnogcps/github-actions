146faa4b44c043c9826809d15ae50e2f39230f6f
Felipe Nogueira <contato.fnog@gmail.com>
2019-07-16 21:44:23 -0300

    Add "How Many Vowels?" solution

---
+++ JavaScript/Edabit/How Many Vowels?/solution.js
@@ -0,0 +1,11 @@
+/*
+ * How Many Vowels? - Edabit
+ *
+ * Create a function that takes a string and returns the number (count)
+ * of vowels contained within it.
+ *
+ * https://edabit.com/challenge/3EQGHyiYTNc9LPmhF
+ * github.com/fnogcps
+ */
+
+countVowels = a => a.match(/[aeiou]/gi).length;
