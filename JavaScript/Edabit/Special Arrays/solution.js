1d6155a03c657b31c5a39b516acfe6ced02f1276
Felipe Nogueira <contato.fnog@gmail.com>
2019-07-17 06:39:56 -0300

    Add "Special Arrays" solution

---
+++ JavaScript/Edabit/Special Arrays/solution.js
@@ -0,0 +1,12 @@
+/*
+ * Special Arrays - Edabit
+ *
+ * An array is special, if every even index contains an even number
+ * and every odd index contains an odd number. Create a function that
+ * returns true if an array is special, and false otherwise.
+ *
+ * https://edabit.com/challenge/wikfKaQm3zP7buRA4
+ * github.com/fnogcps
+ */
+
+isSpecialArray = a => a.length == a.filter(a => !(a % 2)).length * 2;
