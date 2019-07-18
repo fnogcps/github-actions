57a62029f7d16a286083e64750f8d721e0111102
Felipe Nogueira <contato.fnog@gmail.com>
2019-07-18 07:37:06 -0300

    Add "Transform Upvotes" solution

---
+++ JavaScript/Edabit/Transform Upvotes/solution.js
@@ -0,0 +1,14 @@
+/*
+ * Transform Upvotes - Edabit
+ *
+ * Create a function that transforms a string of upvote counts
+ * into an array of numbers. Each k represents a thousand.
+ *
+ * https://edabit.com/challenge/niTEsddTdE4gs4YGX
+ * github.com/fnogcps
+ */
+
+transformUpvotes = a =>
+  a
+    .split(' ')
+    .map(a => (a.endsWith('k') ? parseFloat(a.slice(0, 5)) * 1000 : a * 1));
