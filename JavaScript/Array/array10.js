ca50f7a8aab76bd224ac86f3c2ea4bee72bbda5e
Felipe Nogueira <contato.fnog@gmail.com>
2019-07-18 07:06:45 -0300

    Add array10.js

---
+++ JavaScript/Array/array10.js
@@ -0,0 +1,8 @@
+// Array.concat
+
+const objectOne = [1, 2, 3];
+const objectTwo = [4, 5, 6];
+const objectThree = objectOne.concat(objectTwo);
+
+console.log(objectThree);
+console.log([00].concat([1, 2], [3, 4], [5], [6], [[7, 8]]));
