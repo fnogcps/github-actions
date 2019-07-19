0958f466ab9f1c0615eb5dec8aaa9d6c7a25df04
Felipe Nogueira <contato.fnog@gmail.com>
2019-07-19 05:03:02 -0300

    Add "Sort an Array by String Length" solution

---
+++ JavaScript/Edabit/Sort an Array by String Length/solution.js
@@ -0,0 +1,11 @@
+/*
+ * Sort an Array by String Length - Edabit
+ *
+ * Create a function that takes an array of strings and return an array,
+ * sorted from shortest to longest.
+ *
+ * https://edabit.com/challenge/Gz27eKkMcLarzE5Rj
+ * github.com/fnogcps
+ */
+
+sortByLength = a => a.sort((a, b) => a.length - b.length)
