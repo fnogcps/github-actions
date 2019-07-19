9695b995fa88c9e5d285b0e752edafcbfa350236
Felipe Nogueira <contato.fnog@gmail.com>
2019-07-19 04:28:58 -0300

    Add "Join Two Portions of a Path" solution

---
+++ JavaScript/Edabit/Join Two Portions of a Path/solution.js
@@ -0,0 +1,13 @@
+/*
+ * Join Two Portions of a Path - Edabit
+ *
+ * Write a function that receives two portions of a path
+ * and joins them. The portions will be joined with the "/" separator.
+ * There could be only one separator and if it is not present
+ * it should be added.
+ *
+ * https://edabit.com/challenge/ezv3TCDcgAYhyL3R2
+ * github.com/fnogcps
+ */
+
+joinPath = (a, b) => a.replace(/\//gu, "") + "/" + b.replace(/\//gu, "")
