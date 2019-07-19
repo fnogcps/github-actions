d8f4da8d24d86c0db466425d5389c002bb709725
Felipe Nogueira <contato.fnog@gmail.com>
2019-07-19 05:10:47 -0300

    Add "Remove Every Vowel from a String" solution

---
+++ JavaScript/Edabit/Remove Every Vowel from a String/solution.js
@@ -0,0 +1,11 @@
+/*
+ * Remove Every Vowel from a String - Edabit
+ *
+ * Create a function that takes a string and returns
+ * a new string with all vowels removed.
+ *
+ * https://edabit.com/challenge/DbLp2kHgwQbridSSy
+ * github.com/fnogcps
+ */
+
+removeVowels = a => a.replace(/[aeiou]/gi, '');
