6fa30a5a08716268743df1ee93721e894c3d061b
Felipe Nogueira <contato.fnog@gmail.com>
2019-07-19 04:45:19 -0300

    Add "Bitwise Operations" solution

---
+++ JavaScript/Edabit/Bitwise Operations/solution.js
@@ -0,0 +1,13 @@
+/*
+ * Bitwise Operations - Edabit
+ *
+ * Write 3 functions to calculate the bitwise AND, bitwise OR
+ * and bitwise XOR of two numbers.
+ *
+ * https://edabit.com/challenge/vvuAkYEAArrZvmp6X
+ * github.com/fnogcps
+ */
+
+bitwiseAND = (a, b) => a & b;
+bitwiseOR = (a, b) => a | b;
+bitwiseXOR = (a, b) => a ^ b;
