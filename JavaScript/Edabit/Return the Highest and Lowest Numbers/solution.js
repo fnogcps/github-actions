1e545a8525b0d4b7314fc8e51f34a10848d2b3bc
Felipe Nogueira <contato.fnog@gmail.com>
2019-07-20 06:55:44 -0300

    Add "Return the Highest and Lowest Nu..." solution

---
+++ JavaScript/Edabit/Return the Highest and Lowest Numbers/solution.js
@@ -0,0 +1,11 @@
+/*
+ * Return the Highest and Lowest Numbers - Edabit
+ *
+ * Create a function that accepts a string of space separated numbers
+ * and returns the highest and lowest number (as a string).
+ *
+ * https://edabit.com/challenge/PBXe4hLP3mbbMEFt8
+ * github.com/fnogcps
+ */
+
+highLow = a => `${Math.max(...a.split(' '))} ${Math.min(...a.split(' '))}`;
