adca9761e5810af2c2f8fc93ec06d68a09587bb2
Felipe Nogueira <contato.fnog@gmail.com>
2019-07-20 08:12:31 -0300

    Add "Who's The Oldest?" solution

---
+++ JavaScript/Edabit/Who's The Oldest?/solution.js
@@ -0,0 +1,11 @@
+/*
+ * Who's The Oldest? - Edabit
+ *
+ * Given an object containing the names and ages of a group of people,
+ * return the name of the oldest person.
+ *
+ * https://edabit.com/challenge/vPmDvWoP2wj5KpWZi
+ * github.com/fnogcps
+ */
+
+oldest = a => Object.keys(a).find(b => a[b] == Math.max(...Object.values(a)));
