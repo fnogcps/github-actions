db3aa14d0143e096452b0f9713707601a182e89b
Felipe Nogueira <contato.fnog@gmail.com>
2019-07-23 10:40:52 -0300

    Apply eslint and prettier rules

--- JavaScript/thisKeyword/bind/bind01.js
+++ JavaScript/thisKeyword/bind/bind01.js
@@ -1,13 +1,12 @@
 // bind #1
 
 function funcOne() {
-	console.log(this.x)
-	console.log(this.y)
+  console.log(this.x);
+  console.log(this.y);
 }
 
-
-function funcTwo() { 
-	return { x: 10, y: 20 }
+function funcTwo() {
+  return {x: 10, y: 20};
 }
 
-funcOne.bind(funcTwo())()
+funcOne.bind(funcTwo())();
--- JavaScript/thisKeyword/bind/bind02.js
+++ JavaScript/thisKeyword/bind/bind02.js
@@ -1,9 +1,7 @@
 // bind #2
 
 function funcOne(a, b) {
-	return (this * a * b)
+  return this * a * b;
 }
 
-console.log(
-	funcOne.bind(3, 3)(3)
-)
+console.log(funcOne.bind(3, 3)(3));
