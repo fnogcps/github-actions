25709325268e974a7b9276439d7f560044b6145f
Felipe Nogueira <contato.fnog@gmail.com>
2019-07-28 23:28:05 -0300

    Add "Power of Two" solution

---
+++ JavaScript/Edabit/Power of Two/solution.js
@@ -0,0 +1,11 @@
+ /*
+ * Power of Two - Edabit
+ *
+ * Write a function that returns true if an integer is a power of 2,
+ * and false otherwise.
+ *
+ * https://edabit.com/challenge/x6hfHCyQtbvf6dQDK
+ * github.com/fnogcps
+ */
+
+powerOfTwo = a => Number.isInteger(Math.log2(a));
