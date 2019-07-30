a4b671650ff9f3c89b77cba952c4f0febe49d683
Felipe Nogueira <contato.fnog@gmail.com>
2019-07-29 23:27:10 -0300

    Add "Limit a Number's Value" solution

---
+++ JavaScript/Edabit/Limit a Number's Value/solution.js
@@ -0,0 +1,12 @@
+/*
+ * Limit a Number's Value - Edabit
+ *
+ * Create a function that takes three number arguments — one number as an
+ * input and two additional numbers representing the endpoints of a closed
+ * range — and return the number limited to this range.
+ *
+ * https://edabit.com/challenge/ZwQJM6FsRsWd3Bm6g
+ * github.com/fnogcps
+ */
+
+limitNumber = (a, b, c) => (a > b && a < c ? a : a > b && a > c ? c : b);
