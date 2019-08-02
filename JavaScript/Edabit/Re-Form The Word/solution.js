b0b2413cd69bed0d823616451d5295666d932a3e
Felipe Nogueira <contato.fnog@gmail.com>
2019-08-01 22:02:16 -0300

    Add "Re-Form The Word" solution

---
+++ JavaScript/Edabit/Re-Form The Word/solution.js
@@ -0,0 +1,12 @@
+/*
+ * Re-Form The Word - Edabit
+ *
+ * A word has been split into a left part and a right part.
+ * Re-form the word by adding both halves together, changing the
+ * first character to an uppercase letter.
+ *
+ * https://edabit.com/challenge/KQQ7RMXjSK257GQgy
+ * github.com/fnogcps
+ */
+
+getWord = (a, b) => a[0].toUpperCase() + a.slice(1) + b;
