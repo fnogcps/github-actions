54e416c577550740891f27d4608a2b57bf2224bf
Felipe Nogueira <contato.fnog@gmail.com>
2019-08-02 21:19:51 -0300

    Add "Unlucky 13" solution

---
+++ JavaScript/Edabit/Unlucky 13/solution.js
@@ -0,0 +1,11 @@
+/*
+ * Unlucky 13 - Edabit
+ *
+ * Given a sorted array of numbers, remove any numbers that
+ * are divisible by 13. Return the amended array.
+ *
+ * https://edabit.com/challenge/5232asan2yFuMGnHq
+ * github.com/fnogcps
+ */
+
+unlucky13 = a => a.filter(a => a % 13);
