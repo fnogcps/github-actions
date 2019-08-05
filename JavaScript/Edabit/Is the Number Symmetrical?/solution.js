cc98aaf3647d5bfa84e2d7ae1659275efcfc45da
Felipe Nogueira <contato.fnog@gmail.com>
2019-08-04 23:41:06 -0300

    Add "Is the Number Symmetrical?" solution

---
+++ JavaScript/Edabit/Is the Number Symmetrical?/solution.js
@@ -0,0 +1,16 @@
+/*
+ * Is the Number Less than or Equal to Zero? - Edabit
+ *
+ * Create a function that takes a number as an argument and returns
+ * true or false depending on whether the number is symmetrical or not.
+ * A number is symmetrical when it is the same as its reverse.
+ *
+ * https://edabit.com/challenge/at7jF9Y7SmPGyD92u
+ * github.com/fnogcps
+ */
+
+isSymmetrical = a =>
+  (a + '')
+    .split('')
+    .reverse()
+    .join('') == a;
