dccff993df14c71d5ecc7abc39004ce9d983b92f
Felipe Nogueira <contato.fnog@gmail.com>
2019-08-05 19:29:23 -0300

    Add "Equality of 3 Values" solution

---
+++ JavaScript/Edabit/Equality of 3 Values/solution.js
@@ -0,0 +1,11 @@
+/*
+ * Equality of 3 Values - Edabit
+ *
+ * Create a function that takes three integer arguments (a, b, c)
+ * and returns the number of equal values.
+ *
+ * https://edabit.com/challenge/PKiTjA7SLK99ZZ8GK
+ * github.com/fnogcps
+ */
+
+equal = (a, b, c, d = new Set([a, b, c]).size) => (d == 1 ? 3 : d == 3 ? 0 : 2);
