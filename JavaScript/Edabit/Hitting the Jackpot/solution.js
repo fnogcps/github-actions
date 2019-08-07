e3c6fa0a737e247504b0f0ec3f368d3089696678
Felipe Nogueira <contato.fnog@gmail.com>
2019-08-06 22:41:22 -0300

    Add "Hitting the Jackpot" solution

---
+++ JavaScript/Edabit/Hitting the Jackpot/solution.js
@@ -0,0 +1,12 @@
+/*
+ * Hitting the Jackpot - Edabit
+ *
+ * Create a function that takes in an array (slot machine outcome)
+ * and returns true if all elements in the array are identical,
+ * and false otherwise. The array will contain 4 elements.
+ *
+ * https://edabit.com/challenge/hxyvTffvdT4E238CY
+ * github.com/fnogcps
+ */
+
+testJackpot = a => new Set(a).size < 2;
