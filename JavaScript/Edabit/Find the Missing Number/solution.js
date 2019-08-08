2bb2ff322813808b778257bbcd7ba35906cd082d
Felipe Nogueira <contato.fnog@gmail.com>
2019-08-07 23:00:52 -0300

    Add "Find the Missing Number"

---
+++ JavaScript/Edabit/Find the Missing Number/solution.js
@@ -0,0 +1,11 @@
+/*
+ * Find the Missing Number - Edabit
+ *
+ * Create a function that takes an array of numbers between 1
+ * and 10 (excluding one number) and returns the missing number.
+ *
+ * https://edabit.com/challenge/rgdT9sZ4QhsKhaSw9
+ * github.com/fnogcps
+ */
+
+missingNum = a => 55 - a.reduce((a, b) => a + b)
