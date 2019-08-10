2b9b6165da4c5b14afa5048e9ab535c6856017a8
Felipe Nogueira <contato.fnog@gmail.com>
2019-08-09 23:22:46 -0300

    Add "Number of Arrays in an Array" solution

---
+++ JavaScript/Edabit/Number of Arrays in an Array/solution.js
@@ -0,0 +1,10 @@
+/*
+ * Number of Arrays in an Array - Edabit
+ *
+ * Return the total number of arrays inside a given array.
+ *
+ * https://edabit.com/challenge/TwJghdCGSbDywLZz7
+ * github.com/fnogcps
+ */
+
+numOfSubbarrays = a => a.filter(a => a.length).length;
