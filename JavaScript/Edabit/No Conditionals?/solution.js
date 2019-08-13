95df351041a6850eb7151a628ec07d71adc4fbfa
Felipe Nogueira <contato.fnog@gmail.com>
2019-08-12 23:16:47 -0300

    Add "No Conditionals?" solution

---
+++ JavaScript/Edabit/No Conditionals?/solution.js
@@ -0,0 +1,17 @@
+/*
+ * No Conditionals? - Edabit
+ *
+ * Write a function that returns 0 if the input is 1,
+ * and returns 1 if the input is 0.
+ *
+ * Try completing this challenge without using any:
+ *  - Conditionals
+ *  - Ternary operators
+ *  - Negations
+ *  - Bit operators
+ *
+ * https://edabit.com/challenge/WjXHgXLAvMxNvD6h2
+ * github.com/fnogcps
+ */
+
+flip = a => Math.pow(a, 0) - a;
