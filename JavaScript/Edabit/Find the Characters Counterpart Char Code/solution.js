61cd8af4520869a6840fba8f21b7ed397c95896d
Felipe Nogueira <contato.fnog@gmail.com>
2019-08-12 22:44:12 -0300

    Add "Find the Characters Counterpart ..." solution

---
+++ JavaScript/Edabit/Find the Characters Counterpart Char Code/solution.js
@@ -0,0 +1,14 @@
+/*
+ * Find the Characters Counterpart Char Code - Edabit
+ *
+ * Create a function that takes a single character as an argument and
+ * returns the char code of its lowercased / uppercased counterpart.
+ *
+ * https://edabit.com/challenge/fbaLZPNjTvYtY444B
+ * github.com/fnogcps
+ */
+
+counterpartCharCode = a =>
+  a == a.toLowerCase()
+    ? a.toUpperCase().charCodeAt()
+    : a.toLowerCase().charCodeAt();
