4bca90cf5b4533d57185404cff878c1e61137106
Felipe Nogueira <contato.fnog@gmail.com>
2019-08-12 22:44:11 -0300

    Add "The 3 Programmers Problem" solution

---
+++ JavaScript/Edabit/The 3 Programmers Problem/solution.js
@@ -0,0 +1,13 @@
+/*
+ * The 3 Programmers Problem - Edabit
+ *
+ * You hired three programmers and you (hopefully) pay them.
+ * Create a function that takes three numbers
+ * (the hourly wage of each programmer) and returns the difference
+ * between the highest-paid programmer and the lowest-paid.
+ *
+ * https://edabit.com/challenge/BYZW3yNmG99eLpSdR
+ * github.com/fnogcps
+ */
+
+programmers = (a, b, c) => Math.max(a, b, c) - Math.min(a, b, c);
