49511f2b594e7329f2e0269f0909444a72432fdc
Felipe Nogueira <contato.fnog@gmail.com>
2019-08-12 22:44:05 -0300

    Add "Algebra Sequence — Boxes" solution

---
+++ JavaScript/Edabit/Algebra Sequence — Boxes/solution.js
@@ -0,0 +1,11 @@
+/*
+ * Algebra Sequence — Boxes - Edabit
+ *
+ * Create a function that takes a number (step) as an argument and
+ * returns the amount of boxes in that step of the sequence.
+ *
+ * https://edabit.com/challenge/Q4mYkd8nc8zdFBDE8
+ * github.com/fnogcps
+ */
+
+boxSeq = a => (a % 2 ? a + 2 : a);
