414a669b2d2376962ba45be1c18887b3afc1a363
Felipe Nogueira <contato.fnog@gmail.com>
2019-08-14 21:37:55 -0300

    Add "Find Unique Positive Numbers fro..." solution

---
+++ JavaScript/Edabit/Find Unique Positive Numbers from Array/solution.js
@@ -0,0 +1,11 @@
+/*
+ * Find Unique Positive Numbers from Array - Edabit
+ *
+ * Write a function that takes an array and returns
+ * a new array with unique positive (more than 0) numbers.
+ *
+ * https://edabit.com/challenge/Fx2NdxGzayHpfu8pS
+ * github.com/fnogcps
+ */
+
+uniqueArr = a => [...new Set(a.filter(a => a > 0))];
