02463397ce86aa7f25945f09045ea53cb8c2545e
Felipe Nogueira <contato.fnog@gmail.com>
2019-08-17 21:50:34 -0300

    Add "Fix the Error: Value vs. Referen..." solution

---
+++ JavaScript/Edabit/Fix the Error: Value vs. Reference Types/solution.js
@@ -0,0 +1,11 @@
+/*
+ * Fix the Error: Value vs. Reference Types - Edabit
+ *
+ * Create a function that returns true if two arrays
+ * contain identical values, and false otherwise.
+ *
+ * https://edabit.com/challenge/3jZyJTfsXTXXwAQkA
+ * github.com/fnogcps
+ */
+
+checkEquals = (a, b) => a + 0 == b + 0;
