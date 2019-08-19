cef8cb8e70a77385c51f93b194feaaa716f7bf03
Felipe Nogueira <contato.fnog@gmail.com>
2019-08-18 23:43:35 -0300

    Add "Retrieve the Last N Elements" solution

---
+++ JavaScript/Edabit/Retrieve the Last N Elements/solution.js
@@ -0,0 +1,10 @@
+/*
+ * Retrieve the Last N Elements - Edabit
+ *
+ * Write a function that retrieves the last n elements from an array.
+ *
+ * https://edabit.com/challenge/S6zpG8RwDrxaFEQLE
+ * github.com/fnogcps
+ */
+
+last = (a, b) => (b > a.length ? 'invalid' : a.slice(a.length - b));
