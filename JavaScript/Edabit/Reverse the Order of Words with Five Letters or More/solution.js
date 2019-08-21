6517f87111fa77208eb239ed357bc0cc15b35b72
Felipe Nogueira <contato.fnog@gmail.com>
2019-08-20 23:21:13 -0300

    Add "Reverse the Order of Words with ..." solution

---
+++ JavaScript/Edabit/Reverse the Order of Words with Five Letters or More/solution.js
@@ -0,0 +1,25 @@
+/*
+ * Reverse the Order of Words with Five Letters or More - Edabit
+ *
+ * Write a function that takes a string of one or more words as an
+ * argument and returns the same string, but with all five or more
+ * letter words reversed. Strings passed in will consist of only
+ * letters and spaces. Spaces will be included only when more than one
+ * word is present.
+ *
+ * https://edabit.com/challenge/wPNzJEJebN2mewcqY
+ * github.com/fnogcps
+ */
+
+reverse = a =>
+  a
+    .split(' ')
+    .map(a =>
+      a.split('').length >= 5
+        ? a
+            .split('')
+            .reverse()
+            .join('')
+        : a,
+    )
+    .join(' ');
