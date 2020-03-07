f38793e4f1bd1f27872dd278ed8110a7aa2b4ad3
Felipe Nogueira <contato.fnog@gmail.com>
2020-03-07 07:56:56 -0300

    Fix unit tests

--- JavaScript/Edabit/Calculate Determinant of a 2x2 Matrix/test.js
+++ JavaScript/Edabit/Calculate Determinant of a 2x2 Matrix/test.js
@@ -5,11 +5,35 @@ const e = require('./solution.js');
 
 describe('Calculate Determinant of a 2x2 Matrix (unit tests)\n', () => {
   it('[Test 1] Expected: -2', () =>
-    assert.strictEqual(e.calcDeterminant([[1, 2], [3, 4]]), -2));
+    assert.strictEqual(
+      e.calcDeterminant([
+        [1, 2],
+        [3, 4],
+      ]),
+      -2,
+    ));
   it('[Test 2] Expected: -4', () =>
-    assert.strictEqual(e.calcDeterminant([[5, 3], [3, 1]]), -4));
+    assert.strictEqual(
+      e.calcDeterminant([
+        [5, 3],
+        [3, 1],
+      ]),
+      -4,
+    ));
   it('[Test 3] Expected: 0', () =>
-    assert.strictEqual(e.calcDeterminant([[1, 1], [1, 1]]), 0));
+    assert.strictEqual(
+      e.calcDeterminant([
+        [1, 1],
+        [1, 1],
+      ]),
+      0,
+    ));
   it('[Test 4] Expected: -1', () =>
-    assert.strictEqual(e.calcDeterminant([[1, 1], [1, 0]]), -1));
+    assert.strictEqual(
+      e.calcDeterminant([
+        [1, 1],
+        [1, 0],
+      ]),
+      -1,
+    ));
 });
--- JavaScript/Edabit/Even Odd Partition/test.js
+++ JavaScript/Edabit/Even Odd Partition/test.js
@@ -5,7 +5,10 @@ const e = require('./solution.js');
 
 describe('Even Odd Partition (unit tests)\n', () => {
   it('[Test 1] Expected: [[8, 2, 0], [5, 9]]', () =>
-    assert.deepEqual(e.evenOddPartition([5, 8, 9, 2, 0]), [[8, 2, 0], [5, 9]]));
+    assert.deepEqual(e.evenOddPartition([5, 8, 9, 2, 0]), [
+      [8, 2, 0],
+      [5, 9],
+    ]));
   it('[Test 2] Expected: [[0, 0, 0], [1, 1, 1]]', () =>
     assert.deepEqual(e.evenOddPartition([1, 0, 1, 0, 1, 0]), [
       [0, 0, 0],
