45eef1c8d2faee2c21b60d86f4792b317ac04c91
Felipe Nogueira <contato.fnog@gmail.com>
2020-04-21 20:54:43 -0300

    Add "Diagonal Difference" solution

---
+++ JavaScript/HackerRank/Diagonal Difference/solution.js
@@ -0,0 +1,17 @@
+function diagonalDifference(arr) {
+  let m1 = [],
+    m2 = [],
+    n = arr.length - 1;
+
+  for (let k = n, i = 0; i <= n; i++) {
+    m1.push(arr[i][i]);
+    m2.push(arr[k][i]);
+    k--;
+  }
+
+  let x = m1.reduce((a, b) => a + b, 0);
+  let y = m2.reduce((a, b) => a + b, 0);
+  return Math.abs(x - y);
+}
+
+module.exports = {diagonalDifference};
