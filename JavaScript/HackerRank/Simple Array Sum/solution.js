47dbcbe21c7d540a819555d981ebd7c29818e42c
Felipe Nogueira <contato.fnog@gmail.com>
2020-04-20 21:07:30 -0300

    Add "Simple Array Sum" solution

---
+++ JavaScript/HackerRank/Simple Array Sum/solution.js
@@ -0,0 +1,12 @@
+/*
+ * Simple Array Sum - HackerRank
+ *
+ * hackerrank.com/challenges/simple-array-sum
+ * github.com/fnogcps
+ */
+
+function simpleArraySum(ar) {
+  return ar.reduce((a, b) => a + b);
+}
+
+module.exports = {simpleArraySum};
