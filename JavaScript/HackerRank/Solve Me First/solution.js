835d5e000a43a9e6415ea5c61ce41eac5cd2936a
Felipe Nogueira <contato.fnog@gmail.com>
2020-04-20 21:05:45 -0300

    Add "Solve Me First" solution

---
+++ JavaScript/HackerRank/Solve Me First/solution.js
@@ -0,0 +1,12 @@
+/*
+ * Solve Me First - HackerRank
+ *
+ * hackerrank.com/challenges/solve-me-first
+ * github.com/fnogcps
+ */
+
+function solveMeFirst(a, b) {
+  return a + b;
+}
+
+module.exports = {solveMeFirst};
