994c9670d55d4fd02ed4a818b87d79311a69ba67
Felipe Nogueira <contato.fnog@gmail.com>
2020-04-21 20:51:24 -0300

    Fix unit tests

--- JavaScript/HackerRank/Simple Array Sum/test.js
+++ JavaScript/HackerRank/Simple Array Sum/test.js
@@ -4,7 +4,7 @@ var assert = chai.assert;
 const e = require('./solution.js');
 
 describe('Simple Array Sum (unit tests)\n', () => {
-  it('[Test 1] Expected: [1, 2, 3, 4, 10, 11]', () =>
+  it('[Test 1] Expected: 31', () =>
     assert.strictEqual(e.simpleArraySum([1, 2, 3, 4, 10, 11]), 31));
   it('[Test 2] Expected: 1100', () =>
     assert.strictEqual(
