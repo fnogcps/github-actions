010dacb40ca237f4cb64798348ad6816a5b7b47b
Felipe Nogueira <contato.fnog@gmail.com>
2020-04-20 21:08:12 -0300

    Add unit tests to the "Simple Array Sum" solution

---
+++ JavaScript/HackerRank/Simple Array Sum/test.js
@@ -0,0 +1,14 @@
+var chai = require('chai');
+var assert = chai.assert;
+
+const e = require('./solution.js');
+
+describe('Simple Array Sum (unit tests)\n', () => {
+  it('[Test 1] Expected: [1, 2, 3, 4, 10, 11]', () =>
+    assert.strictEqual(e.simpleArraySum([1, 2, 3, 4, 10, 11]), 31));
+  it('[Test 2] Expected: 1100', () =>
+    assert.strictEqual(
+      e.simpleArraySum([338, 65, 713, 595, 428, 610, 728, 573, 871, 868]),
+      5789,
+    ));
+});
