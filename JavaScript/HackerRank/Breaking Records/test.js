132c89377fcb573fa9b4418fce4113067b35ac54
Felipe Nogueira <contato.fnog@gmail.com>
2020-04-22 22:06:32 -0300

    Fix unit tests

--- JavaScript/HackerRank/Breaking Records/test.js
+++ JavaScript/HackerRank/Breaking Records/test.js
@@ -4,12 +4,12 @@ var assert = chai.assert;
 const e = require('./solution.js');
 
 describe('Breaking Records (unit tests)\n', () => {
-  it('[Test 1] Expected: [10, 5, 20, 20, 4, 5, 2, 25, 1]', () =>
+  it('[Test 1] Expected: [2, 4]', () =>
     assert.deepEqual(e.breakingRecords([10, 5, 20, 20, 4, 5, 2, 25, 1]), [
       2,
       4,
     ]));
-  it('[Test 2] Expected: [3, 4, 21, 36, 10, 28, 35, 5, 24, 42]', () =>
+  it('[Test 2] Expected: [4, 0]', () =>
     assert.deepEqual(e.breakingRecords([3, 4, 21, 36, 10, 28, 35, 5, 24, 42]), [
       4,
       0,
